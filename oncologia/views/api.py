cec12b171bc700143fd020c8b9050385eb9aa1be
sr-pato <felipe.adeildo@proton.me>
2024-01-31 17:42:21 -0300

    Add Toast and Implement API to pendency management

--- oncologia/views/api.py
+++ oncologia/views/api.py
@@ -1,8 +1,39 @@
+from http import HTTPStatus
+
 from flask import Blueprint, jsonify, request
+
+from oncologia.extensions.database import db
+from oncologia.extensions.models import Pendency, PendencyStatus
+from oncologia.utils import login_required
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
 
 @bp.put("/pendency/<pendency_id>")
-def update_pendency(pendency_id):
-    return jsonify({"pendency_id": pendency_id})
+@login_required
+def update_pendency(pendency_id: int):
+    pendency = Pendency.query.get_or_404(pendency_id)
+    body = request.get_json()
+    status = body.get("status")
+
+    if status is None:
+        return (
+            jsonify({"status": False, "message": "Status is required"}),
+            HTTPStatus.BAD_REQUEST,
+        )
+
+    try:
+        status_enum = PendencyStatus(status)
+    except ValueError:
+        return (
+            jsonify({"status": False, "message": "Invalid status"}),
+            HTTPStatus.BAD_REQUEST,
+        )
+
+    pendency.status = status_enum
+    db.session.commit()
+
+    return (
+        jsonify({"status": True, "message": "Pendency updated"}),
+        HTTPStatus.OK,
+    )
--- oncologia/views/home.py
+++ oncologia/views/home.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from flask import Blueprint, render_template
 
-from oncologia.extensions.models import Pendency
+from oncologia.extensions.models import Pendency, PendencyStatus
 from oncologia.utils import login_required
 
 bp = Blueprint("home", __name__)
@@ -12,7 +12,8 @@ bp = Blueprint("home", __name__)
 @login_required
 def index():
     pendencies = Pendency.query.filter(
-        Pendency.due_date < datetime.now(), Pendency.status == 1
+        Pendency.due_date < datetime.now(),
+        Pendency.status == PendencyStatus.pending,
     ).all()
     context = {"pendencies": pendencies}
     return render_template("home/index.html", **context)
